b53a5937eb6e9f7cf60f1918d38aba22c4ffe90e
Shawn Brown <stbst2@student.mtaloy.edu>
2022-05-07 21:07:58 -0400

    player movement and temp player sprite added

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //Movement
+    public float speed;
+    public float jump;
+    float moveVelocity;
+    BoxCollider2D bc; 
+
+    //Grounded Vars
+    bool isGrounded = true;
+
+    void Update()
+    {
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (isGrounded)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                isGrounded = false;
+            }
+        }
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+    //Check if Grounded
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        isGrounded = true;
+        Debug.Log("Collided");
+    }
+}
